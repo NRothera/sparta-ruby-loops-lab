08989afd96d27a3c139e376f5384443de928fecc
NRothera <31472463+NRothera@users.noreply.github.com>
2017-12-13 09:24:34 +0000

    readme

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # sparta-ruby-loops-lab
+
+
+Ruby loop practice in order to become fluent in Ruby language
--- loops-starter-code/loops.rb
+++ loops-starter-code/loops.rb
@@ -1,16 +1,16 @@
 # return an array of reversed words
 def reverse_array_of_strings words
-  reversed_array = words.map do |x|
-    x.reverse
+  reversed_array = words.map do |word|
+    word.reverse
   end
 end
 
 #return all the even numbers less than the given number
 def even_numbers_less_than num
   even_array = []
-  for x in 0...num
-    if x % 2 == 0
-      even_array.push(x)
+  for number in 0...num
+    if number % 2 == 0
+      even_array.push(number)
     end
   end
   return even_array
@@ -18,8 +18,8 @@ end
 
 # return the average of all numbers in an array
 def average numbers
-  ave = numbers.reduce 0 do |total, numbers|
-    sum = (total + numbers)
+  ave = numbers.reduce 0 do |total, number|
+    sum = (total + number)
   end
   ave / numbers.length.to_f
 end
